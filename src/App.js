e862c97648c348861f099e16e504569f362a449e
Anderson Sipe <amsipe@gmail.com>
2017-10-17 05:53:28 -0400

    css updates

--- src/App.js
+++ src/App.js
@@ -9,10 +9,15 @@ class App extends Component {
     this.state = {
       products: [],
       orders: [],
-      cart: []
+      cart: {
+        total: 0,
+        customerID: null,
+        items: []
+      }
       
     }
     this.getProducts = this.getProducts.bind(this);
+    this.handleCartAdd = this.handleCartAdd.bind(this);
    
   }
   componentWillMount(){
@@ -28,7 +33,6 @@ class App extends Component {
           throw Error(err);
         }
         
-        console.log(JSON.parse(res.text));
         this.setState({
           products: JSON.parse(res.text)
         });
@@ -51,24 +55,20 @@ class App extends Component {
 
   handleOrderSubmit(){
     //TODO: replace send object with cart state
+    var cartCopy = _.cloneDeep(this.state.cart);
     var url = 'http://localhost:5000/orders';
     request
       .post(url)
       .send({
-        total: 13.99,
-        customer: 6,
-        items: [
-          [1,1,3.99],
-          [2,1,5.99],
-          [4,1,7.85]
-          ]})
+        total: cartCopy.total,
+        customer: cartCopy.customerID,
+        items: cartCopy.items
+       })
       .set('accept','json')
       .end((err,res) => {
         if(err){
           throw Error(err);
         }
-
-        console.log(res.body);
         this.getOrders();
       })
   }
@@ -76,7 +76,6 @@ class App extends Component {
   handleOrderDelete(){
       
       var stateCopy = _.map(this.state.orders,_.cloneDeep);
-      console.log(stateCopy);
       var url = 'http://localhost:5000/orders/' + stateCopy[0].OrderID;
       request
         .del(url)
@@ -85,23 +84,30 @@ class App extends Component {
           if(err){
             throw Error(err);
           }
-  
-          console.log(res.body);
           this.getOrders();
         })
+  }
+
+  handleCartAdd(product,count = 1){
+    console.log(product);
+    var cartCopy = _.cloneDeep(this.state.cart);
+    var newItem = [product.ProductID,count,product.Price];
+    console.log(newItem);
+    cartCopy.items.push(newItem);
+    //TODO: replace random number for customerID with better process
+    //random number is assigned to customer if not assigned 
+    cartCopy.customerID = (!cartCopy.customerID) ? _.random(1,10) : cartCopy.customerID;
+    cartCopy.total = _.sumBy(cartCopy.items, (item) => {
+      return item[2];
+    })
+    this.setState({
+      cart: cartCopy
+    })
+
   }
   render() {
 
     //TODO: replace orders with a seperate page and component
-
-    // var products = _.map(this.state.products,(product,index) => {
-    //   return <li key={index} >
-    //           <img src={"/img/" + product.ImgURL} alt={product.Name}/>
-    //           <div>{product.Name}</div>
-    //           <div>$ {product.Price}</div>
-    //           <button>Add to Cart</button>
-    //          </li>
-    // })
 
     var orders = _.map(this.state.orders,(order,index) => {
       return <li key={index} >{order.OrderID}</li>
@@ -112,22 +118,19 @@ class App extends Component {
       <div>
         <p>{cartItems}</p>
         <input type="text" name="newOrder"/>
-        <button onClick={() => {this.handleOrderSubmit()}}>Submit</button>
+        <button onClick={() => {this.handleOrderSubmit()}}>Submit Order</button>
         <input type="text" name="deleteOrder"/>
         <button onClick={() => {this.handleOrderDelete()}}>Delete</button>
-        <ul>{this.state.products.map((product,index) => {
+        <ul className="product-list">{this.state.products.map((product,index) => {
           return (
           <Product 
             key={index} 
-            source={product.ImgURL}
-            name={product.Name}
-            price={product.Price}
-            description={product.Description}
-            productID={product.ProductID}
+            product={product}
+            onCartAdd={this.handleCartAdd}
              />
           )
         })}</ul>
-        <ul>{orders}</ul>
+        <ul className="orders-list">{orders}</ul>
       </div>
     );
   }
@@ -136,33 +139,16 @@ class App extends Component {
 class Product extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      cart: []
-    }
-  }
-  //TODO: move state push to global state
-  handleAddCart(){
-    console.log(this.props);
-
-    var stateCopy = _.map(this.state.cart,_.cloneDeep);
-    console.log(stateCopy);
-    var newItem = [this.props.productID,1,this.props.price]
-    stateCopy.push(newItem);
-    console.log(stateCopy);
-    this.setState({
-      cart: [...stateCopy]
-    })
-    console.log(this.state)
   }
 
   render(){
 
     return (
-      <li>
-      <img src={"/img/" + this.props.source} alt={this.props.name}/>
-      <div>{this.props.name}</div>
-      <div>$ {this.props.price}</div>
-      <button onClick={() => {this.handleAddCart()}}>Add to Cart</button>
+      <li className="product-item">
+      <img src={"/img/" + this.props.product.ImgURL} alt={this.props.product.Name}/>
+      <div className="product-name">{this.props.product.Name}</div>
+      <div className="product-price">$ {this.props.product.Price}</div>
+      <button className="add-button" onClick={() => {this.props.onCartAdd(this.props.product)}}>Add to Cart</button>
      </li>
     );
   }
